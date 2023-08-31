a555a656abe3e2bf59ea02621f7bbb33cdd21e19
ponmathi <ponmathittl@gmail.com>
2023-08-31 18:39:33 +0530

    items visible

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import classes from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -28,12 +30,24 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals=()=>{
-    const mealsList=DUMMY_MEALS.map((meal)=><li>{meal.name}</li>);
+    const mealsList=DUMMY_MEALS.map((meal)=>{
         return (
-            <section className={classes.meals}>
-                <ul>{mealsList}</ul> 
-            </section>
+            <MealItem 
+                key={meal.id}
+                name={meal.name} 
+                description={meal.description}
+                price={meal.price}
+            />
         )
+    });
+  
+    return (
+        <section className={classes.meals}>
+            <Card>
+                <ul>{mealsList}</ul> 
+            </Card>
+        </section>
+    );
 }
   
 export default AvailableMeals;
